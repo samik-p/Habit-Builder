744f7d1a69b9248f7956052c519e58988c131adc
Samik Pradhan <samikpradhan@gmail.com>
2025-03-13 15:10:36 -0700

    removed django test implementation from App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,19 +7,6 @@ import MenuAppBar from './components/MUI-AppBar'
 import OutlinedCard from './components/Card'
 
 function App() {
-  const [response, setResponse] = useState('')
-
-  const fetchData = () => {
-    fetch('http://localhost:8000/test_endpoint/')
-      .then(response => response.json())
-      .then(data => {
-        setResponse(data.message);
-      });
-  }
-
-  const handleButtonClick = () => {
-    fetchData();
-  }
 
   return (
     <>
@@ -34,16 +21,6 @@ function App() {
       <div id="addButton">
         <FloatingActionButtonSize></FloatingActionButtonSize>
       </div>
-
-      <h1>Test</h1>
-      <button onClick={handleButtonClick}>Submit</button>
-
-      {response &&
-        <>
-          <h2>Response:</h2>
-          {response}
-        </>
-      }
     </>
   )
 }
