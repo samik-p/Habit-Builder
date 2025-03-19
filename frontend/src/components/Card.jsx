5203abfe919f47e16b0d8e495719e94dcc49ed04
Sree Chinta <sreechinta16@gmail.com>
2025-03-18 17:03:58 -0700

    added checkbox for marking a habit as complete and updated readme

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,72 +1,88 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import CheckIcon from '@mui/icons-material/Check';
+import ToggleButton from '@mui/material/ToggleButton';
+
 import './Card.css'
 
 
-const card = (
-  <React.Fragment>
-    <CardContent>
-      <div className="box">
-        <Box
-          sx={{
-            bgcolor: '#0362fc',
-            height: "10vh",
-            width: "30vw"
-
-
-          }}
-        >
-          <div className='box1'>
-            <Box
-              sx={{
-                bgcolor: '#ab1a1f',
-                height: "4.5vh",
-                width: "2.5vw"
-
-              }}
-            >
-            </Box>
-          </div>
-
-
-        </Box>
-        <Box
-          sx={{
-            bgcolor: '#c390f0',
-            height: "10vh",
-            width: "10vw"
-          }}
-        >
-        </Box>
-        <Box
-          sx={{
-            bgcolor: '#f23860',
-            height: "10vh",
-            width: "50vw"
-
-          }}
-
-        >
-
-        </Box>
-        <CardActions>
-          <Button size="small">Learn More</Button>
-        </CardActions>
-      </div>
-    </CardContent>
-
-  </React.Fragment>
-);
-
 export default function OutlinedCard() {
+  const [selected, setSelected] = React.useState(false)
   return (
-    <Box sx={{ maxHeight: "15vh", width: "80vw", maxWidth: "1920px" }}>
-      <Card variant="outlined">{card}</Card>
+    <Box sx={{ maxHeight: "15vh", width: "60vw", maxWidth: "1920px" }}>
+      <Card variant="outlined">
+        <React.Fragment>
+          <CardContent>
+            <div className="box">
+              <Box
+                sx={{
+                  bgcolor: '#0362fc',
+                  height: "10vh",
+                  width: "50vw",
+
+
+                }}
+              >
+                <div className='Icon'>
+                  <Box
+                    sx={{
+                      bgcolor: '#ab1a1f',
+                      height: "8vh",
+                      width: "8vh",
+                      marginLeft: "2%",
+                      marginTop: "1.5%"
+
+
+                    }}
+                  >
+                  </Box>
+                </div>
+
+
+              </Box>
+              <Box
+                sx={{
+                  bgcolor: '#c390f0',
+                  height: "10vh",
+                  width: "20vw"
+                }}
+              >
+              </Box>
+              <Box
+                sx={{
+                  bgcolor: '#f23860',
+                  height: "10vh",
+                  width: "10vw"
+
+                }}
+
+              >
+
+                <ToggleButton
+                  value="check"
+                  selected={selected}
+                  onChange={() => setSelected((prevSelected) => !prevSelected)}
+                  sx={{
+                    bgcolor: '#ffffff',
+                    marginTop: '18%',
+
+                  }}
+                >
+                  <CheckIcon />
+                </ToggleButton>
+
+              </Box>
+            </div>
+          </CardContent>
+
+        </React.Fragment>
+      </Card>
     </Box>
+
   );
 }
+
+
+
